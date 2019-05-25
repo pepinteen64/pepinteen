9fd9a0ea1497ddb796b2d897800b7ff00f73255e
Diana <dm.graur@gmail.com>
2019-05-25 15:10:06 +0300

    test

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/test', (req, res) => res.send('Hello PepinTeen!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
